72a56582138a5e59758509e2f4e72dab24a6c432
mjaumi <mjaumi2864@gmail.com>
2022-04-29 14:48:49 +0600

    mongoDB connected

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tqizw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    console.log('byteware DB connected');
+    client.close();
+});
+
+
 // base API
 app.get('/', (req, res) => {
     res.send('ByteWare server running');
