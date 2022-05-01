399fb1f8b14eb1233bd01552f3c3c422052368fe
mjaumi <mjaumi2864@gmail.com>
2022-05-01 22:54:28 +0600

    simple get API added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,16 +9,31 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+// mongoDB connection here
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tqizw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    console.log('byteware DB connected');
-    client.close();
-});
+
+async function run() {
+    try {
+        await client.connect();
+        const itemsCollection = client.db('inventory').collection('items');
+
+        console.log('DB connected');
+
+        // GET ITEMS API
+        app.get('/items', async (req, res) => {
+            const query = {};
+            const cursor = itemsCollection.find(query);
+            const items = await cursor.toArray();
+            res.send(items);
+        });
+
+    } finally {
+
+    }
+}
+
+run().catch(console.dir);
 
 
 // base API
